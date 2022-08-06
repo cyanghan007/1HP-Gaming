85d7a24c35647dbcf455dec2e8b9230d726c1c45
Chan Yang <chan_yang@hotmail.com>
2022-08-06 12:51:35 -0400

    installed React, setup engine and created first route

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+// Requiring express
 const express = require('express')
 require('dotenv').config()
 const app = express()
+// Setting up port
 const port = process.env.PORT || 3003
 
+//setting up our views
+app.set('view engine', 'jsx')
+//Initialzing my engine
+app.engine('jsx', require('express-react-views').createEngine()) 
+
+// Home page 
+app.get('/', (req, res) => {
+    res.send('Welcome to 1HP-Gaming')
+})
 
 
 // My port
